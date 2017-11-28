c16c0ed1917c38de0a82785f806985c57cabc75d
salminnella <salminnella@gmail.com>
2017-11-27 23:11:58 -0800

    drawing line on movement - still offset from actual touch

--- app/src/main/java/com/demo/salminnella/drawdots/MainActivity.java
+++ app/src/main/java/com/demo/salminnella/drawdots/MainActivity.java
@@ -1,13 +1,95 @@
 package com.demo.salminnella.drawdots;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    ImageView imageView;
+    Bitmap bitmap;
+    Canvas canvas;
+    Paint paint;
+    float downx = 0, downy = 0, upx = 0, upy = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) this.findViewById(R.id.imageView1);
+
+        Display currentDisplay = getWindowManager().getDefaultDisplay();
+        float dw = currentDisplay.getWidth();
+        float dh = currentDisplay.getHeight();
+
+        bitmap = Bitmap.createBitmap((int) dw, (int) dh,
+                Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        paint = new Paint();
+        paint.setColor(Color.GREEN);
+        imageView.setImageBitmap(bitmap);
+
+        imageView.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+
+        float startX = 0;
+        float startY = 0;
+        float endX = 0;
+        float endY = 0;
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                startX=event.getX();
+                startY=event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                endX = event.getX();
+                endY = event.getY();
+                canvas.drawLine(startX,startY,endX,endY, paint);
+                imageView.invalidate();
+                startX=endX;
+                startY=endY;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+            default:
+                break;
+        }
+
+//        switch (action) {
+//            case MotionEvent.ACTION_DOWN:
+//                downx = event.getX();
+//                downy = event.getY();
+//                break;
+//            case MotionEvent.ACTION_MOVE:
+//                canvas.drawCircle(downx, downy,1,paint);
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                upx = event.getX();
+//                upy = event.getY();
+//                canvas.drawLine(downx, downy, upx, upy, paint);
+//                imageView.invalidate();
+//                break;
+//            case MotionEvent.ACTION_CANCEL:
+//                break;
+//            default:
+//                break;
+//        }
+        return true;
+
     }
 }
